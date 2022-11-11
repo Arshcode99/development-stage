0bf0e9fcf0cfb03b77c21686e96cb2de3b7e512e
arshahdul99 <Arshahdul99@gmail.com>
2022-11-11 20:05:10 +0530

    footer and fast start component added

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import styles from './components.module.css';
+import Link from 'next/link';
+import { BsLinkedin } from 'react-icons/bs'
+import { BsInstagram } from 'react-icons/bs'
+import { BsWhatsapp } from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>
-      I'm Footer Bhai!
-    </div>
+    <>
+      <div className={styles.Footer}>
+        <div className={styles.leftfoot}>
+          <Link className={styles.a} href="/">Home</Link>
+          <Link className={styles.a} href="/">Courses</Link>
+          <Link className={styles.a} href="/">Careers</Link>
+          <Link className={styles.a} href="/">Community</Link>
+        </div>
+        <div className={styles.rightfoot}>
+          <p>Copyright 2022 Reactjsofficial.com | All Rights Reserved</p>
+        </div>
+        </div>
+    </>
   );
 }
 
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import '../styles/globals.css'
 import Header from '../components/navbar'
+import Quickstart from '../components/Quickstart'
+import Footer from '../components/footer'
 
 function MyApp({ Component, pageProps }) {
   return<>
   <Header></Header>
   <Component {...pageProps} />
+  <Quickstart />
+  <Footer />
   </> 
 }
 
