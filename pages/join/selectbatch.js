fe9ea2e0c789e7e1dbf2738a4c26460ae4173771
Arshcode99 <Arshcode99@gmail.com>
2022-12-04 19:32:30 +0530

    yup

--- pages/join/selectbatch.js
+++ pages/join/selectbatch.js
@@ -29,40 +29,7 @@ const SelectBatch = () => {
     }
     );
     if (res) {
-      var options = {
-        "key": "rzp_test_4mWGndDk6F7am7", // Enter the Key ID generated from the Dashboard
-        "amount": "1000000", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
-        "currency": "INR",
-        "name": "Acme Corp",
-        "description": "Test Transaction",
-        "image": "https://example.com/your_logo",
-        "order_id": "order_KnqxNlmKuSmn2Z", //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-        "callback_url": "https://eneqd3r9zrjok.x.pipedream.net/",
-        "prefill": {
-            "name": "Gaurav Kumar",
-            "email": "gaurav.kumar@example.com",
-            "contact": "9999999999"
-        },
-        "notes": {
-            "address": "Razorpay Corporate Office"
-        },
-        "theme": {
-            "color": "#3399cc"
-        }
-    };
-    var rzp1 = new Razorpay(options);
-rzp1.on('payment.failed', function (response){
-        alert(response.error.code);
-        alert(response.error.description);
-        alert(response.error.source);
-        alert(response.error.step);
-        alert(response.error.reason);
-        alert(response.error.metadata.order_id);
-        alert(response.error.metadata.payment_id);
-});
-
-    var rzp1 = new Razorpay(options);
-    rzp1.open();
+      window.location.href = "https://pages.razorpay.com/reactjsofficial-payment";
     }
     else {
       alert("Maybe Something went wrong! Try Again... 🤔")
