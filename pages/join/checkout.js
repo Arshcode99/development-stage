86c6a290ad8b2bd992eea9f7404224d286650e5a
Arshcode99 <Arshcode99@gmail.com>
2022-12-06 20:05:33 +0530

    change

--- pages/join/checkout.js
+++ pages/join/checkout.js
@@ -1,5 +1,6 @@
 import React from 'react';
-// import Script from 'next/script';
+import PayImg from '../../public/firewire.svg';
+import { useState, useEffect } from 'react';
 import styles from '../../styles/checkout.module.css';
 
 const Checkout = () => {
@@ -26,7 +27,7 @@ const Checkout = () => {
         const res = await initializeRazorpay();
     
         if (!res) {
-          alert("Razorpay SDK Failed to load");
+          alert("It Seems Like You're Offline Or Something Wrong From Our Side, If You're ONLINE and still seeing this message than please contact us at our contact page!");
           return;
         }
     
@@ -45,9 +46,10 @@ const Checkout = () => {
           image: "https://cdn.razorpay.com/logos/KnpjU4FBX4UW0r_medium.png",
           handler: function (response) {
             // Validate payment at server - using webhooks is a better idea.
-            alert(response.razorpay_payment_id);
-            alert(response.razorpay_order_id);
-            alert(response.razorpay_signature);
+            alert("Thank You! You Will Be Redirected To Our Pro Slack Community Now! Sit Back And Relax😉...")
+            // alert(response.razorpay_payment_id);
+            // alert(response.razorpay_order_id);
+            // alert(response.razorpay_signature);
           },
           prefill: {
             name: "Test Person",
@@ -60,6 +62,12 @@ const Checkout = () => {
         paymentObject.open();
       };
 
+      //Let's Take fetch data Dynamically!
+      var today = new Date().toLocaleDateString().slice(0,10);
+      const [time, setTime] = useState(new Date());
+      useEffect(() => {
+        setInterval(() => setTime(new Date()), 30000);
+      }, []);
 
     return (
         <>
@@ -67,30 +75,57 @@ const Checkout = () => {
             <section className={styles.Checkout}>
                 <div className={styles.twocol}>
                     <div className={styles.left}>
-                        <h2>Invoice</h2>
-                        <table className={styles.table}>
-                            <tr>
+                        <h2>Billing Details</h2>
+                        <form className={styles.form}>
+                            <label htmlFor="Name">Name*</label>
+                            <input type="text" name="Name" required className={styles.input} placeholder="Enter Your Name" />
+                            <label htmlFor="Email">Email*</label>
+                            <input type="email" name="Email" required className={styles.input} placeholder="Enter Your Email" />
+                            <label htmlFor="Address">Address*</label>
+                            <input type="text" name="Address" required className={styles.input} placeholder="Enter Your Address" />
+                            <label htmlFor="Phone">Phone*</label>
+                            <input type="tel" name="Phone" required className={styles.input} placeholder="Enter Your Phone No." />
+                            <label htmlFor="CupponCode">Cuppon Code*</label>
+                            <input type="text" name="CupponCode" className={styles.input} placeholder="N/A" disabled />
+                            <hr />
+                            <h2>Overview</h2>
+                            <div className={styles.overflow}>
+                            <table className={styles.table}>
+                                <tr>
                                 <th className={styles.th}>Course</th>
+                                <th className={styles.th}>Date</th>
+                                <th className={styles.th}>Time</th>
                                 <th className={styles.th}>Price</th>
                                 <th className={styles.th}>GST</th>
+                                <th className={styles.th}>Cuppon Discount</th>
+                                <th className={styles.th}>Discount</th>
                                 <th className={styles.th}>Total</th>
-                            </tr>
-                            <tr>
-                                <td className={styles.td}>FRONTEND DEVELOPMENT <br /> WITH REACT SPECIALIZATION - TRAINING PROGRAM </td>
-                                <td className={styles.td}>₹38,099/-</td>
-                                <td className={styles.td}>18%</td>
-                                <td className={styles.td}>₹44,956/-</td>
-                            </tr>
-                        </table>
-                        <br />
-                        <b>Rajeev Ranjan Singh</b>
-                        <h5>Signature (CEO & FOUNDER)</h5><br />
-                    </div>
-                    <hr />
-                    <div className={styles.right}>
-                        <h3>Amount Payable</h3>
-                        <h6>₹44,956/-</h6><br />
-                        <button onClick={makePayment} className={styles.btn}>Pay Now</button>
+                                </tr>
+                                <tr>
+                                    <td className={styles.td}>FRONTEND DEVELOPMENT <br /> WITH REACT SPECIALIZATION - TRAINING PROGRAM</td>
+                                    <td className={styles.td}>{today}</td>
+                                    <td className={styles.td}>{time.toLocaleString('en-US', {
+                                        hour: 'numeric',
+                                        minute: 'numeric',
+                                        hour12: true,
+                                    })}</td>
+                                    <td className={styles.td}>₹38,099/-</td>
+                                    <td className={styles.td}>18%</td>
+                                    <td className={styles.td}>N/A</td>
+                                    <td className={styles.td}>52%</td>
+                                    <td className={styles.td}>₹44,956/-</td>
+                                </tr>
+                            </table>
+                            </div>
+                            <b>Note* : Any False Information Can Result in Conflict and if found Your Course purchase will Be terminated!
+                                <br />
+                                You Will Be Redirected To Invoice Page After Successfull Payment!
+                            </b>
+                            <hr />
+                            <h3>Amount Payable</h3>
+                        <h6>₹44,956/-</h6>
+                        <button type='submit' onClick={makePayment} className={styles.btn}>Pay Now</button>
+                        </form>
                     </div>
                 </div>
             </section>
